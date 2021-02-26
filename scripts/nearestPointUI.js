6f2e7fd39bdb0d2be37cfbe51aabb8cb1f34aca9
LABMATT <matt42764@gmail.com>
2021-02-26 13:14:40 +1100

    Anchor CPUI
    - modifed location status of center node.

--- scripts/nearestPointUI.js
+++ scripts/nearestPointUI.js
@@ -4,15 +4,18 @@ class npui {
     this.canvasName = canvasName;
     this.nodeArray = []; // Array that all nodes are stored into.
     this.snd = 10; // Standard node distance.
-    this.ancorLoc = "center";
+    this.anchor;
 
   }
 
 
   // Sets a node as needing no perant and having no angel from other nodes, insted provides a lcoation on screen for that ancor node to be placed.
-  setAnchor()
+  setAnchor(anc, x, y)
   {
 
+    this.anchor = anc;
+    this.anchor.x = x;
+    this.anchor.y = y;
   }
 
   newNode(obj) {
@@ -24,6 +27,13 @@ class npui {
   // get node <nodeID> gets that node from the node array. Returns node then use this to change settings of this node.
   getNode(id)
   {
+
+    if(id == "anchor")
+    {
+
+      return this.anchor;
+    } else
+    {
     nodeArray.forEach(element => {
       if(element.nodeID == id)
       {
@@ -32,9 +42,13 @@ class npui {
       }
     });
   }
+  }
   
   
   // Display canvas puts this all into action.
+  // Gets the xy points of every element of the menu starting with anchor.
+  // plots a line from the xy point to the next to make the tree.
+  // re-covers over all xy points and draws the ui element.
   display(on)
   {
     var canvas = document.getElementById(this.canvasName);
@@ -45,7 +59,45 @@ class npui {
 
     if(display == true)
     { 
+     ctx.beginPath();
+
+     ctx.lineWidth = size;
+     ctx.strokeStyle = colour;
+
+     var forceRefactor = false;
+
+     // If the anchor is ment to be centred on any axis then set that up for the current canvs postion.
+     if(this.anchor.x == "center" || this.anchor.y == "center")
+     {
+       if(this.anchor.x == "center")
+       {
+        this.anchor.x = menuCenterX;
+       }
+       if(this.anchor.y == "center")
+       {
+        this.anchor.y = menuCenterY;
+       }
+
+
+     }
+     
+     //ctx.lineTo(x, y);
       
+      nodeArray.forEach(currentNode => {
+        if(forceRefactor == true)
+        {
+          currentNode.refactorPerant(); //https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+        }
+
+        ctx.moveTo(currentNode.x, currentNode.y); // Start the achor postion at where we set it, we will draw a line from here.
+      });
+
+
+      ctx.fillStyle = 'black';
+      ctx.stroke();
+      ctx.fill();
+      ctx.closePath();
+
     } else
     {
 
@@ -61,11 +113,20 @@ class node {
     this.nodeID = id;         // The ID that can be used to refreance this node.
     this.perantID = perant;   // The ID of the node this node is attached to.
     this.angle = 0;           // Where stright to top of screen is 0 degreese. What angle from there does your node point off in?
+    this.x;
+    this.y;
+    this.perantX;
+    this.perantY;
+
+    this.refactorPerant(); // Get XY of perant
 
     this.bottomtxt = "";      // Text displayed underneeth node.
     this.toptxt = "";         // Text displayed ontop of node.
     this.centertxt = "";      // Text displayed in the center of node.
     this.textcolour = "white";
+
+    this.lineColour = "defualt";
+    this.linesize = "default";
 
     this.size = 10;           // Node size, how thicc node is.
     this.colour = "orange";   // base colour of the node.
@@ -73,6 +134,30 @@ class node {
     this.image = null;        // The image displayed on node.
     this.func = null;         // The function that will be exacuted when you click on this node.
 
+    
+  }
+
+  // Gets the perants XY number 
+  // Then works out where it should be using trig and sets its own postion to that.
+  refactorPerant()
+  {
+    var pidxy = getNode(this.perantID); // Get the perants node.
+    this.perantX = pidxy.x;
+    this.perantY = pidxy.y;
+
+    
+  }
+
+  // Sets the colour of the line going to the node.
+  setLineColour(lc)
+  {
+    this.lineColour = lc;
+  }
+
+  // Sets the size of the line going to the node.
+  setLineSize(ls)
+  {
+    this.linesize = ls
   }
 
   //text displayed at postion stated.
