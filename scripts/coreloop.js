50d9badde99dcc9962d1ec00085d6d261eff3b0d
LABMATT <matt42764@gmail.com>
2022-10-03 02:14:42 +1100

    SetupCore
    - Added comments related to the init process.
    - Created Diagrams chart

--- scripts/coreloop.js
+++ scripts/coreloop.js
@@ -1,3 +1,5 @@
+// Core loop is called repeately with a time interval inbetween thats set as the refresh rate. As set in init.js
+
 function coreloop()
 {
     redrawCanvas();
--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,18 @@
+// Info on setup process
+// https://drive.google.com/file/d/1in8FTHWITefyq-Uk_VloN6mcc08LeLFP/view?usp=sharing
+
 var refreshrate = 6;
 
 function init()
 {
 console.log("Setting up Dolude.");
-initCANVAS();
-resize(); 
-disableRightMenu();
-mousehold();
-shortcutManger();
-createHost();
-joinMenu();
+initCANVAS();         // Setup basic canvas vars
+resize();             // Resizes sets canvis size to the new size of the window before redrawing the canvas.
+disableRightMenu();   // Disables the context menu that chrome would usaly display when right clicked.
+mousehold();          // Mouse hold is an event that knows when the mouse is held down. this is an init var call.
+shortcutManger();     // This attaches an event to the 
+createHost();         // Sets up host menu however is hidden.
+joinMenu();           // Setsup join menue however is hidden.
 setInterval(coreloop, refreshrate); // This runs the core loop that updates the canvas
 console.log("INIT COMPLETE");
 }
